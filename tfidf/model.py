9d5dcc94f22266f4135dc0e7552a0b3e233dbb3b
daviduvalle <david.uvalle@gmail.com>
2018-11-06 21:08:00 -0800

    Scaffolding of tf-idf

--- tfidf/model.py
+++ tfidf/model.py
@@ -4,7 +4,14 @@ import pandas as pd
 # Load context from the test data set and convert them into
 # the tf-idf vocabulary
 
-test_data = pd.read_csv('../data/test.csv')
+test_dataframe = pd.read_csv('../data/test.csv')
+context = test_dataframe['Context'].values.tolist()
 
-print test_data.shape
+context_vectorizer = TfidfVectorizer()
+# tokenize and build vocab
+context_vectorizer.fit(context)
 
+for c in context:
+    context_vector = context_vectorizer.transform([c])
+    # this vector should be used to run cosine distance
+    # against responses and collect data to determine recall@1-9
