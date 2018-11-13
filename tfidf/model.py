1eda84f25c4e6b69a9b873edd4913e2f92350360
daviduvalle <david.uvalle@gmail.com>
2018-11-12 21:14:26 -0800

    Test computing cosine similarity between context, ground truth and the first distractor

--- tfidf/model.py
+++ tfidf/model.py
@@ -1,17 +1,24 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 
 # Load context from the test data set and convert them into
 # the tf-idf vocabulary
 
+# TODO: will need to clean that up, remove stop words and __eou__ / __eot__
 test_dataframe = pd.read_csv('../data/test.csv')
 context = test_dataframe['Context'].values.tolist()
+ground_truth = test_dataframe['Ground Truth Utterance'].values.tolist()
+distractor = test_dataframe['Distractor_0'].values.tolist()
 
+# Vectorize context, ground truth, and distractor
 context_vectorizer = TfidfVectorizer()
-# tokenize and build vocab
 context_vectorizer.fit(context)
 
-for c in context:
-    context_vector = context_vectorizer.transform([c])
-    # this vector should be used to run cosine distance
-    # against responses and collect data to determine recall@1-9
+for i in range(len(context)):
+    context_vector = context_vectorizer.transform([context[i]])
+    ground_truth_vector = context_vectorizer.transform([ground_truth[i]])
+    distractor_vector = context_vectorizer.transform([distractor[i]])
+    print cosine_similarity(context_vector, ground_truth_vector)
+    print cosine_similarity(context_vector, distractor_vector)
+    break
