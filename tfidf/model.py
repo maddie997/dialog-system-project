31d967c8c2222d529c25805e7b05f3773b69ccb1
daviduvalle <david.uvalle@gmail.com>
2018-11-13 21:16:30 -0800

    Computing recall@1 with tf-idf

--- tfidf/model.py
+++ tfidf/model.py
@@ -14,11 +14,21 @@ distractor = test_dataframe['Distractor_0'].values.tolist()
 # Vectorize context, ground truth, and distractor
 context_vectorizer = TfidfVectorizer()
 context_vectorizer.fit(context)
+context_len = len(context)
 
-for i in range(len(context)):
+print 'Total number of context lines: ' + str(context_len)
+
+correct_responses = 0;
+
+for i in range(context_len):
     context_vector = context_vectorizer.transform([context[i]])
     ground_truth_vector = context_vectorizer.transform([ground_truth[i]])
     distractor_vector = context_vectorizer.transform([distractor[i]])
-    print cosine_similarity(context_vector, ground_truth_vector)
-    print cosine_similarity(context_vector, distractor_vector)
-    break
+    context_groundtruth_score = cosine_similarity(context_vector, ground_truth_vector)
+    context_distractor_score = cosine_similarity(context_vector, distractor_vector)
+
+    if (context_groundtruth_score >= context_distractor_score):
+        correct_responses = correct_responses + 1
+
+recall_at_one = (correct_responses * 100) / context_len
+print 'Recall@1 with tf-idf over the test set {0:.2f} %'.format(recall_at_one)
