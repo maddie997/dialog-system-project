77a2ead609f9550f5a5905154778fda6190d9e9a
dprof <manishpandey4096@gmail.com>
2018-12-16 22:17:28 -0800

    Updates to hparams file

--- lstm/chatbot-retrieval/udc_hparams.py
+++ lstm/chatbot-retrieval/udc_hparams.py
@@ -9,19 +9,25 @@ tf.flags.DEFINE_integer(
 
 # Model Parameters
 tf.flags.DEFINE_integer("embedding_dim", 100, "Dimensionality of the embeddings")
-tf.flags.DEFINE_integer("rnn_dim", 256, "Dimensionality of the RNN cell")
+tf.flags.DEFINE_integer("rnn_dim", 128, "Dimensionality of the RNN cell")
+tf.flags.DEFINE_boolean("rnn_multilayer", False, "If True multilayer model")
+tf.flags.DEFINE_integer("rnn_layers", 1, "Number of RNN/LSTM layers")
+tf.flags.DEFINE_boolean("rnn_bidirectional", False, "If True bidirectional model")
+#Note - Model cant be multilayer and bidirectional at the same time
+tf.flags.DEFINE_boolean("use_dual_encoder", True, "If True use dual encoder else manhattan")
 tf.flags.DEFINE_integer("max_context_len", 160, "Truncate contexts to this length")
 tf.flags.DEFINE_integer("max_utterance_len", 80, "Truncate utterance to this length")
 
 # Pre-trained embeddings
-tf.flags.DEFINE_string("glove_path", None, "Path to pre-trained Glove vectors")
-tf.flags.DEFINE_string("vocab_path", None, "Path to vocabulary.txt file")
+tf.flags.DEFINE_string("glove_path", "/home/mpandey/ubuntu_dialogue/cs230_project/dialog-system-project/lstm/chatbot-retrieval/data/glove.6B.100d.txt", "Path to pre-trained Glove vectors")
+tf.flags.DEFINE_string("vocab_path", "/home/mpandey/ubuntu_dialogue/cs230_project/dialog-system-project/lstm/chatbot-retrieval/data/vocabulary.txt", "Path to vocabulary.txt file")
 
 # Training Parameters
 tf.flags.DEFINE_float("learning_rate", 0.001, "Learning rate")
 tf.flags.DEFINE_integer("batch_size", 64, "Batch size during training")
 tf.flags.DEFINE_integer("eval_batch_size", 8, "Batch size during evaluation")
 tf.flags.DEFINE_string("optimizer", "Adam", "Optimizer Name (Adam, Adagrad, etc)")
+tf.flags.DEFINE_string("logdir", "logs", "log directory name")
 
 FLAGS = tf.flags.FLAGS
 
@@ -36,9 +42,14 @@ HParams = namedtuple(
     "max_utterance_len",
     "optimizer",
     "rnn_dim",
+    "rnn_layers",
+    "rnn_multilayer",
+    "rnn_bidirectional",
     "vocab_size",
     "glove_path",
-    "vocab_path"
+    "vocab_path",
+    "use_dual_encoder",
+    "logdir"
   ])
 
 def create_hparams():
@@ -53,4 +64,10 @@ def create_hparams():
     max_utterance_len=FLAGS.max_utterance_len,
     glove_path=FLAGS.glove_path,
     vocab_path=FLAGS.vocab_path,
-    rnn_dim=FLAGS.rnn_dim)
+    rnn_dim=FLAGS.rnn_dim,
+    rnn_multilayer=FLAGS.rnn_multilayer,
+    rnn_layers=FLAGS.rnn_layers,
+    rnn_bidirectional=FLAGS.rnn_bidirectional,
+    use_dual_encoder=FLAGS.use_dual_encoder,
+    logdir=FLAGS.logdir
+  )
